819b34f3f4bf360c0513565422fc6f90d897a280
박상준 <sjunpark@itembay.com>
2025-06-30 22:10:54 +0900

    Initialize Spring Boot project with Prometheus, Grafana, and H2 database support. Add Docker Compose setup, Gradle configuration, and project structure.

---
+++ src/test/java/com/monitoring/sapg/SpringActuatorPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoring.sapg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringActuatorPrometheusGrafanaApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
